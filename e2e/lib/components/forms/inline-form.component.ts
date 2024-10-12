fe59a92153dd5e012e50a74c389948be0be02010
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-10-12 23:20:48 +0200

    Add InlineFormComponent class for managing inline forms

---
+++ e2e/lib/components/forms/inline-form.component.ts
@@ -0,0 +1,26 @@
+import { Page } from '@playwright/test';
+import { FormComponent } from '@components/forms/form.component';
+
+class InlineFormComponent extends FormComponent {
+  constructor(page: Page) {
+    super(page, "nb-card:has-text('Inline form')");
+  }
+
+  async fillName(name: string) {
+    await this.fillInputByRole('textbox', 'Jane Doe', name);
+  }
+
+  async fillEmail(email: string) {
+    await this.fillInputByRole('textbox', 'Email', email);
+  }
+
+  async checkRememberMe() {
+    await this.checkCheckboxByRole('checkbox', 'Remember me');
+  }
+
+  async submit() {
+    await this.clickButton('Submit');
+  }
+}
+
+export { InlineFormComponent };
