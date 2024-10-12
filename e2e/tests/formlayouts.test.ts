1a7526956891cd533511d88af9c611da90b4a415
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-10-12 17:08:36 +0200

    Add test for submitting inline form in FormLayoutsPage

---
+++ e2e/tests/formlayouts.test.ts
@@ -0,0 +1,16 @@
+import { test, StartPage } from '@fixtures/base-fixtures';
+
+test.describe('Form Layouts', () => {
+  test.use({ startPage: StartPage.FormLayouts });
+
+  test('Should submit inline form successfully', async ({
+    formLayoutsPage,
+  }) => {
+    await formLayoutsPage.submitInlineFormWithOptions(
+      'Jane Doe',
+      'jane.doe@example.com',
+      true
+    );
+
+  });
+});
