cc5470fcce1ba9fc73e5aa6a694c71e5b713de0a
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-10-12 23:22:24 +0200

    Add FormComponent class for managing form interactions

---
+++ e2e/lib/components/forms/form.component.ts
@@ -0,0 +1,39 @@
+import { Page, Locator } from '@playwright/test';
+import { BaseComponent } from '@components/base.component';
+
+export abstract class FormComponent extends BaseComponent {
+  protected formLocator: Locator;
+
+  constructor(page: Page, formSelector: string) {
+    super(page);
+    this.formLocator = page.locator(formSelector);
+  }
+
+  async fillInputByRole(role: 'textbox', name: string, value: string) {
+    await super.fillInputByRole(role, name, value, this.formLocator);
+  }
+
+  async fillInputByLabel(label: string, value: string) {
+    await super.fillInputByLabel(label, value, this.formLocator);
+  }
+
+  async fillInputByPlaceholder(placeholder: string, value: string) {
+    await super.fillInputByPlaceholder(placeholder, value, this.formLocator);
+  }
+
+  async clickButton(buttonName: string) {
+    await super.clickButton(buttonName, this.formLocator);
+  }
+
+  async checkCheckboxByLabel(label: string) {
+    await super.checkCheckboxByLabel(label, this.formLocator);
+  }
+
+  async checkCheckboxByRole(role: 'checkbox', name: string) {
+    await super.checkCheckboxByRole(role, name, this.formLocator);
+  }
+
+  async getText(selector: string): Promise<string> {
+    return await super.getText(selector, this.formLocator);
+  }
+}
