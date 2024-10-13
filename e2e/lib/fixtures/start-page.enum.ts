49768b37612cc46ff159402246de2063658f65e9
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-10-13 18:13:11 +0200

    Add StartPage enum for e2e fixtures

---
+++ e2e/lib/fixtures/start-page.enum.ts
@@ -0,0 +1,10 @@
+enum StartPage {
+  IoTDashboard = 'IoT Dashboard',
+  FormLayouts = 'Form Layouts',
+  DatePicker = 'Date Picker',
+  Toastr = 'Toastr',
+  SmartTable = 'Smart Table',
+  //TODO add more pages
+}
+
+export { StartPage };
