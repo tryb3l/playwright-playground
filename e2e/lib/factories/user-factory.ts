9c43674cd4ee9969a31a101a14deee310c69a6f2
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-10-13 18:14:33 +0200

    Refactor UserFactory class to remove unused code

--- e2e/lib/factories/user-factory.ts
+++ e2e/lib/factories/user-factory.ts
@@ -1,18 +1,1 @@
-export class UserFactory {
-  static createUser(overrides?: Partial<User>): User {
-    return {
-      username: `user_${}`,
-      email: `user_${}@example.com`,
-      password: 'Password123',
-      // Include other user properties
-      ...overrides,
-    };
-  }
-}
-
-interface User {
-  username: string;
-  email: string;
-  password: string;
-  // Other properties...
-}
+export class UserFactory {}
