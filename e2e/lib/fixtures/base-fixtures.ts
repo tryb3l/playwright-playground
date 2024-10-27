eb91716ae65a74f8bc3569b535c1f8da145194f5
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-10-27 23:59:52 +0100

    add fixture to track errors in console

--- e2e/lib/fixtures/base-fixtures.ts
+++ e2e/lib/fixtures/base-fixtures.ts
@@ -1,4 +1,4 @@
-import { test as base } from '@playwright/test';
+import { test as base, ConsoleMessage } from '@playwright/test';
 import { PageManager } from '@pages/page-manager';
 import { StartPage } from '@fixtures/start-page.enum';
 import { FormLayoutsPage } from '@pages/form-layouts.page';
@@ -9,7 +9,38 @@ export type TestOptions = {
   formLayoutsPage: FormLayoutsPage;
 };
 
-export const test = base.extend<TestOptions>({
+export const test = base.extend<TestOptions & { trackConsoleErrors: boolean }>({
+  trackConsoleErrors: [true, { option: true }],
+  page: async ({ page, trackConsoleErrors }, use, testInfo) => {
+    const errors: string[] = [];
+
+    if (trackConsoleErrors) {
+      const listener = (msg: ConsoleMessage) => {
+        if (msg.type() === 'error') {
+          errors.push(msg.text());
+        }
+      };
+      page.on('console', listener);
+      await use(page);
+
+      page.off('console', listener);
+
+      if (errors.length > 0) {
+        await testInfo.attach('Console Errors', {
+          body: errors.join('\n'),
+          contentType: 'text/plain',
+        });
+
+        testInfo.status = 'failed';
+        testInfo.error = new Error(
+          `Console errors detected: \n${errors.join('\n')}`
+        );
+      }
+    } else {
+      await use(page);
+    }
+  },
+
   startPage: [StartPage.IoTDashboard, { option: true }],
   pageManager: async ({ page, startPage }, use) => {
     const pageManager = new PageManager(page);
