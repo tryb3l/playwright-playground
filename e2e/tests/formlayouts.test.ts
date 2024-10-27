97f74d5a2c0c3f748891463bc78035580901f9cc
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-10-28 00:00:24 +0100

    update test with new fixture

--- e2e/tests/formlayouts.test.ts
+++ e2e/tests/formlayouts.test.ts
@@ -1,7 +1,7 @@
 import { test, StartPage } from '@fixtures/base-fixtures';
 
 test.describe('Form Layouts', () => {
-  test.use({ startPage: StartPage.FormLayouts });
+  test.use({ startPage: StartPage.FormLayouts, trackConsoleErrors: true });
 
   test('Should submit inline form successfully', async ({
     formLayoutsPage,
