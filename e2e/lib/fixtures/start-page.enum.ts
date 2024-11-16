15fbc65c91fcdd924a9182eba415470f9007c371
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-11-16 22:43:31 +0100

    refactor page enums

--- e2e/lib/fixtures/start-page.enum.ts
+++ e2e/lib/fixtures/start-page.enum.ts
@@ -1,9 +1,20 @@
 enum StartPage {
   IoTDashboard = 'IoT Dashboard',
-  FormLayouts = 'Form Layouts',
-  DatePicker = 'Date Picker',
-  Toastr = 'Toastr',
-  SmartTable = 'Smart Table',
+  FormLayouts = 'Forms > Form Layouts',
+  DatePicker = 'Forms > Date Picker',
+  Dialog = 'Modal & Overlays > Dialog',
+  Window = 'Modal & Overlays > Window',
+  Popover = 'Modal & Overlays > Popover',
+  Toastr = 'Modal & Overlays > Toastr',
+  Tooltip = 'Modal & Overlays > Tooltip',
+  Calendar = 'Extra Components > Calendar',
+  Echarts = 'Charts > Echarts',
+  SmartTable = 'Tables & Data > Smart Table',
+  TreeGrid = 'Tables & Data > Tree Grid',
+  Login = 'Auth > Login',
+  Register = 'Auth > Register',
+  RequestPassword = 'Auth > Request Password',
+  ResetPassword = 'Auth > Reset Password',
   //TODO add more pages
 }
 
