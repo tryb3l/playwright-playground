2ecf0738f30d5179b816b1aee4c6f10b134cdc71
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-11-16 22:41:52 +0100

    update navigation

--- e2e/lib/components/navbar/navigation.component.ts
+++ e2e/lib/components/navbar/navigation.component.ts
@@ -1,50 +1,30 @@
 import { Page } from '@playwright/test';
 import { BaseComponent } from '@components/base.component';
-import { StartPage } from '@fixtures/base-fixtures';
+
 
 class NavigationComponent extends BaseComponent {
   constructor(page: Page) {
     super(page);
   }
 
-  private readonly menuActions: { [key in StartPage]: () => Promise<void> } = {
-    [StartPage.IoTDashboard]: async () => {
-      await this.page.goto('/');
-    },
-    [StartPage.FormLayouts]: async () => {
-      await this.expandMenu('Forms');
-      await this.clickByText('Form Layouts');
-    },
-    [StartPage.DatePicker]: async () => {
-      await this.expandMenu('Forms');
-      await this.clickByText('Datepicker');
-    },
-    [StartPage.Toastr]: async () => {
-      await this.expandMenu('Modal & Overlays');
-      await this.clickByText('Toastr');
-    },
-    [StartPage.SmartTable]: async () => {
-      await this.expandMenu('Tables & Data');
-      await this.clickByText('Smart Table');
-    },
-    //TODO add more pages
-  };
+  async navigateToSection(sectionPath: string) {
+    const menuItems = sectionPath.split(' > ');
 
-  async navigateToSection(section: StartPage) {
-    const action = this.menuActions[section];
-    if (action) {
-      await action();
-    } else {
-      throw new Error(`Unknown section: ${section}`);
-    }
-  }
+    for (const menuItemName of menuItems) {
+      const menuItem = this.page.getByRole('link', { name: menuItemName });
 
-  private async expandMenu(menuItemName: string) {
-    const menuItem = this.page.getByTitle(menuItemName);
-    const expanded = await menuItem.getAttribute('aria-expanded');
+      //Check if the menu item is expandable
+      const isExpandable =
+        (await menuItem.getAttribute('aria-expanded')) !== null;
 
-    if (expanded === 'false') {
-      await menuItem.click();
+      if (isExpandable) {
+        const expanded = await menuItem.getAttribute('aria-expanded');
+        if (expanded === 'false') {
+          await menuItem.click();
+        }
+      } else {
+        await menuItem.click();
+      }
     }
   }
 }
