c2cc91591b6f63c099d585e315e1641225dd0e20
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-11-17 18:49:43 +0100

    refactor form layouts test to improve error handling and structure

--- e2e/tests/formlayouts.test.ts
+++ e2e/tests/formlayouts.test.ts
@@ -1,19 +1,30 @@
-import { test, StartPage } from '@fixtures/base-fixtures';
+import { createTestForStartPage, expect } from '@fixtures/custom-test';
+import { StartPage } from '@fixtures/start-page.enum';
 
-test.describe('Form Layouts', () => {
-  test.use({
-    startPage: StartPage.FormLayouts || 'defaultPage',
-    trackConsoleErrors: true,
-    ignoreConsoleErrors: ['Error msg to ignore during test suit execution'],
-  });
+const test = createTestForStartPage(StartPage.FormLayouts);
 
-  test('Should submit inline form successfully', async ({
-    formLayoutsPage,
-  }) => {
-    await formLayoutsPage.submitInlineFormWithOptions(
+// Example of how to use ignoreConsoleErrors to ignore specific console errors
+test.use({
+  ignoreConsoleErrors: [
+    'Specific error message to ignore',
+    'Another error to ignore',
+  ],
+  authenticated: false,
+});
+
+test.describe('Form Layouts Tests', () => {
+  test('Submit Inline Form', async ({ pageObject, consoleErrorsTracker }) => {
+    await pageObject.submitInlineFormWithOptions(
       'Jane Doe',
       'jane.doe@example.com',
       true
     );
+
+    //Assertions
+    //const successMessage = await pageObject.getSuccessMessage();
+    //expect(successMessage).toContain('Form submitted successfully');
+    // Check for console errors
+    const consoleErrors = consoleErrorsTracker.getErrors();
+    expect(consoleErrors.length).toBe(0);
   });
 });
