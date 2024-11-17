106ef8c929c0d0eff907b5eaf6912e4a6915f11a
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-11-17 18:48:57 +0100

    add logging to FormComponent methods for better traceability

--- e2e/lib/components/forms/base/base.form.component.ts
+++ e2e/lib/components/forms/base/base.form.component.ts
@@ -10,30 +10,37 @@ export abstract class FormComponent extends BaseComponent {
   }
 
   async fillInputByRole(role: 'textbox', name: string, value: string) {
+    this.logger.info(`Filling input by role`, { role, name, value });
     await super.fillInputByRole(role, name, value, this.formLocator);
   }
 
   async fillInputByLabel(label: string, value: string) {
+    this.logger.info(`Filling input by label`, { label, value });
     await super.fillInputByLabel(label, value, this.formLocator);
   }
 
   async fillInputByPlaceholder(placeholder: string, value: string) {
+    this.logger.info(`Filling input by placeholder`, { placeholder, value });
     await super.fillInputByPlaceholder(placeholder, value, this.formLocator);
   }
 
   async clickButton(buttonName: string) {
+    this.logger.info(`Clicking button by role name`, { buttonName });
     await super.clickButtonByRoleName(buttonName, this.formLocator);
   }
 
   async checkCheckboxByLabel(label: string) {
+    this.logger.info(`Checking checkbox by label`, { label });
     await super.checkCheckboxByLabel(label, this.formLocator);
   }
 
   async checkCheckboxByRole(role: 'checkbox', name: string) {
+    this.logger.info(`Checking checkbox by role`, { role, name });
     await super.checkCheckboxByRole(role, name, this.formLocator);
   }
 
   async getText(selector: string): Promise<string> {
+    this.logger.info(`Getting text from element`, { selector });
     return await super.getText(selector, this.formLocator);
   }
 }
