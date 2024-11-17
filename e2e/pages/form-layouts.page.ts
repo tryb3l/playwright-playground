9eb18440195d35029f0dda0a228c7999c3681556
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-11-17 18:48:07 +0100

    add logging to FormLayoutsPage for form submission tracking

--- e2e/pages/form-layouts.page.ts
+++ e2e/pages/form-layouts.page.ts
@@ -1,19 +1,25 @@
 import { Page } from '@playwright/test';
 import { InlineFormComponent } from '@components/forms/inline-form.component';
 import { GridFormComponent } from '@components/forms/grid-form.component';
+import { Logger } from '@utils/logger';
 
 class FormLayoutsPage {
+  private logger: Logger;
+
   constructor(
     page: Page,
     private inlineForm: InlineFormComponent = new InlineFormComponent(page),
     private gridForm: GridFormComponent = new GridFormComponent(page)
-  ) {}
+  ) {
+    this.logger = new Logger('FormLayoutsPage');
+  }
 
   async submitInlineFormWithOptions(
     name: string,
     email: string,
     rememberMe: boolean
   ) {
+    this.logger.info(`Submitting inline form`, { name, email, rememberMe });
     await this.inlineForm.fillName(name);
     await this.inlineForm.fillEmail(email);
     if (rememberMe) {
@@ -27,6 +33,7 @@ class FormLayoutsPage {
     password: string,
     optionText: string
   ) {
+    this.logger.info(`Submitting inline form`, { name, email, rememberMe });
     await this.gridForm.fillEmail(email);
     await this.gridForm.fillPassword(password);
     await this.gridForm.selectOption(optionText);
