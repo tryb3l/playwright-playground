7f9f4b3187c4dbff42ca4f6f2304c00d791bd5cb
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-11-17 16:32:04 +0100

    small tweaks

--- e2e/lib/fixtures/page-mapping.ts
+++ e2e/lib/fixtures/page-mapping.ts
@@ -1,14 +1,13 @@
-import { FormLayoutsPage } from '@pages/form-layouts.page'
+import { FormLayoutsPage } from '@pages/form-layouts.page';
 import { DatePickerPage } from '@pages/date-picker.page';
 import { StartPage } from '@fixtures/start-page.enum';
 
 export const startPageClassMap = {
-    [StartPage.FormLayouts]: FormLayoutsPage,
-    [StartPage.DatePicker]: DatePickerPage
-    //TODO: add more mappings
+  [StartPage.FormLayouts]: FormLayoutsPage,
+  [StartPage.DatePicker]: DatePickerPage,
+  //TODO: add more mappings
 };
 
 export type StartPageClassMap = typeof startPageClassMap;
-
 
 export { StartPage };
