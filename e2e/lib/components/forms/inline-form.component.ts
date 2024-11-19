7e0756ed485d91eee09127286df906394406e6ea
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-11-20 00:50:24 +0100

    remove logger implementation from InlineFormComponent to simplify code

--- e2e/lib/components/forms/inline-form.component.ts
+++ e2e/lib/components/forms/inline-form.component.ts
@@ -1,44 +1,24 @@
-import { Page, Locator } from '@playwright/test';
+import { Page } from '@playwright/test';
 import { FormComponent } from '@components/forms/base/base.form.component';
-import { Logger } from '@utils/logger';
 
 class InlineFormComponent extends FormComponent {
-  private logger: Logger;
-
   constructor(page: Page) {
-    super(page, "nb-card:has-text('Inline form')");
-    this.logger = new Logger('InlineFormComponent');
+    super(page, 'nb-card.inline-form-card');
   }
 
   async fillName(name: string) {
-    this.logger.info('Filling name field', { field: 'Name', value: name });
-    this.logger.debug(
-      `Filling input by role 'textbox' with name 'Name' and value '${name}'`
-    );
-    await this.fillInputByRole('textbox', 'Jane Doe', name);
+    await this.fillInputByPlaceholder('Jane Doe', name);
   }
 
   async fillEmail(email: string) {
-    this.logger.info('Filling email field', { field: 'Email', value: email });
-    this.logger.debug(
-      `Filling input by role 'textbox' with name 'Email' and value '${email}'`
-    );
-    await this.fillInputByRole('textbox', 'Email', email);
+    await this.fillInputByPlaceholder('Email', email);
   }
 
   async checkRememberMe() {
-    this.logger.info('Checking "Remember me" checkbox', {
-      field: 'Remember me',
-    });
-    this.logger.debug(
-      `Checking checkbox by role 'checkbox' with label 'Remember me'`
-    );
-    await this.checkCheckboxByRole('checkbox', 'Remember me');
+    await this.checkCheckboxByLabel('Remember me');
   }
 
   async submit() {
-    this.logger.info('Submitting inline form');
-    this.logger.debug(`Clicking button by role 'button' with name 'Submit'`);
     await this.clickButton('Submit');
   }
 }
