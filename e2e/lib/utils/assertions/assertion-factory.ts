3864290b858ce878be341c1e74fcaeff261e69f3
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-11-20 21:11:17 +0100

    add assertions factory for creating various assertion types in tests

---
+++ e2e/lib/utils/assertions/assertion-factory.ts
@@ -0,0 +1,20 @@
+import { type Page } from '@playwright/test';
+import { FormAssertions } from './form-assertions';
+import { DatePickerAssertions } from './datepicker-assertions';
+import { TableAssertions } from './table-assertions';
+
+export class AssertionsFactory {
+  constructor(private page: Page) {}
+
+  createFormAssertions() {
+    return new FormAssertions(this.page);
+  }
+
+  createDatePickerAssertions() {
+    return new DatePickerAssertions(this.page);
+  }
+
+  createTableAssertions() {
+    return new TableAssertions(this.page);
+  }
+}
