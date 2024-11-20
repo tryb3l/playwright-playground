f2c97fd1fdb16160edc4d9992de1150993939a94
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-11-20 02:31:22 +0100

    refactor click and fill methods to support additional locator options

--- e2e/lib/components/base.component.ts
+++ e2e/lib/components/base.component.ts
@@ -12,16 +12,73 @@ export abstract class BaseComponent {
     this.logger = new Logger(componentName || this.constructor.name);
   }
 
-  // Generic methods applicable to all components
-
+  // Base locator methods
   @LogExecution
-  async click(selector: string) {
-    await this.context.click(selector);
+  protected getByRole(role: string, options?: { name?: string }) {
+    return this.context.getByRole(role, options);
   }
 
   @LogExecution
-  async fill(selector: string, value: string) {
-    await this.context.locator(selector).fill(value);
+  protected getByLabel(label: string): Locator {
+    return this.context.getByLabel(label);
+  }
+
+  @LogExecution
+  protected getByPlaceholder(placeholder: string): Locator {
+    return this.context.getByPlaceholder(placeholder);
+  }
+
+  @LogExecution
+  protected getByTestId(testId: string): Locator {
+    return this.context.getByTestId(testId);
+  }
+
+  // Generic methods applicable to all components
+
+  @LogExecution
+  async click(
+    selector: string | Locator,
+    options?: {
+      byRole?: string;
+      byLabel?: string;
+      byTestId?: string;
+    }
+  ): Promise<void> {
+    let locator: Locator;
+
+    if (options?.byRole) {
+      locator = this.getByRole(options.byRole, { name: selector as string });
+    } else if (options?.byLabel) {
+      locator = this.getByLabel(selector as string);
+    } else if (selector instanceof Locator) {
+      locator = selector;
+    } else {
+      locator = this.context.locator(selector as string);
+    }
+    await locator.click();
+  }
+
+  @LogExecution
+  async fill(
+    selector: string | Locator,
+    value: string,
+    options?: { byRole?: string; byLabel?: string; byPlaceholder?: string }
+  ): Promise<void> {
+    let locator: Locator;
+
+    if (options?.byRole) {
+      locator = this.getByRole(options.byRole, { name: selector as string });
+    } else if (options?.byLabel) {
+      locator = this.getByLabel(selector as string);
+    } else if (options?.byPlaceholder) {
+      locator = this.getByPlaceholder(selector as string);
+    } else if (selector instanceof Locator) {
+      locator = selector;
+    } else {
+      locator = this.context.locator(selector as string);
+    }
+
+    await locator.fill(value);
   }
 
   @LogExecution
