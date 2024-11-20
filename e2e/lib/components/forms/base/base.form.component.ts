93a8304b0c66d780bb2b9a4e077ecb000d2290f4
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-11-20 02:33:00 +0100

    refactor FormComponent to integrate ButtonComponent and CheckboxComponent

--- e2e/lib/components/forms/base/base.form.component.ts
+++ e2e/lib/components/forms/base/base.form.component.ts
@@ -1,39 +1,45 @@
 import { Page, Locator } from '@playwright/test';
 import { BaseComponent } from '@components/base.component';
 import { LogExecution } from '@utils/decorators';
+import { ButtonComponent } from '@components/button/button.component';
+import { CheckboxComponent } from '@components/checkbox/checkbox.component';
 
 export abstract class FormComponent extends BaseComponent {
   protected formLocator: Locator;
+  protected buttonComponent: ButtonComponent;
+  protected checkboxComponent: CheckboxComponent;
 
   constructor(page: Page, formSelector: string) {
     super(page);
     this.formLocator = page.locator(formSelector);
     this.context = this.formLocator;
+    this.buttonComponent = new ButtonComponent(page, formSelector);
+    this.checkboxComponent = new CheckboxComponent(page, formSelector);
   }
 
   @LogExecution
   async fillInputByRole(role: 'textbox', name: string, value: string) {
-    await this.context.getByRole(role, { name }).fill(value);
+    await this.fill(name, value, { byRole: role });
   }
 
   @LogExecution
   async fillInputByLabel(label: string, value: string) {
-    await this.context.getByLabel(label).fill(value);
+    await this.fill(label, value, { byLabel: label });
   }
 
   @LogExecution
   async fillInputByPlaceholder(placeholder: string, value: string) {
-    await this.context.getByPlaceholder(placeholder).fill(value);
+    await this.fill(placeholder, value, { byPlaceholder: placeholder });
   }
 
   @LogExecution
   async clickButton(buttonName: string) {
-    await this.context.getByRole('button', { name: buttonName }).click();
+    await this.buttonComponent.clickButtonByRole('button', buttonName);
   }
 
   @LogExecution
   async checkCheckboxByLabel(label: string) {
-    await this.context.getByLabel(label).check({ force: true });
+    await this.checkboxComponent.checkByLabel(label);
   }
 
   @LogExecution
