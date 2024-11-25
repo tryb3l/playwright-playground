9bddd055437042fdbde891d78dca44da531321f1
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-11-25 01:32:51 +0100

    extend eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,22 @@
 module.exports = {
-  extends: ['google', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended', 'google', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
+    project: './tsconfig.json',
+    createDefaultProgram: true,
   },
+  plugins: ['@typescript-eslint'],
   env: {
     node: true,
     browser: true,
   },
   rules: {
     'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'require-jsdoc': 'off',
+    'valid-jsdoc': 'off',
   },
 };
