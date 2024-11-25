b967cb13e483437ffbdb9cf483fa95efd43e9030
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-11-25 01:35:21 +0100

    update import statement for Page type in assertion-factory

--- e2e/lib/utils/assertions/assertion-factory.ts
+++ e2e/lib/utils/assertions/assertion-factory.ts
@@ -1,4 +1,4 @@
-import { type Page } from '@playwright/test';
+import type { Page } from '@playwright/test';
 import { FormAssertions } from './form-assertions';
 import { DatePickerAssertions } from './datepicker-assertions';
 import { TableAssertions } from './table-assertions';
