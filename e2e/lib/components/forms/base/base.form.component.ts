2269f12b541054ee77e72a17027aedecbb5233f4
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-11-25 01:37:15 +0100

    refactor BaseFormComponent to use type imports for Page and Locator, and remove LogExecution decorators

--- e2e/lib/components/forms/base/base.form.component.ts
+++ e2e/lib/components/forms/base/base.form.component.ts
@@ -1,6 +1,5 @@
-import { Page, Locator } from '@playwright/test';
+import type { Page, Locator } from '@playwright/test';
 import { BaseComponent } from '@components/base.component';
-import { LogExecution } from '@utils/decorators';
 import { ButtonComponent } from '@components/button/button.component';
 import { CheckboxComponent } from '@components/checkbox/checkbox.component';
 
@@ -17,32 +16,26 @@ export abstract class FormComponent extends BaseComponent {
     this.checkboxComponent = new CheckboxComponent(page, formSelector);
   }
 
-  @LogExecution
   async fillInputByRole(role: 'textbox', name: string, value: string) {
     await this.fill(name, value, { byRole: role });
   }
 
-  @LogExecution
   async fillInputByLabel(label: string, value: string) {
     await this.fill(label, value, { byLabel: label });
   }
 
-  @LogExecution
   async fillInputByPlaceholder(placeholder: string, value: string) {
     await this.fill(placeholder, value, { byPlaceholder: placeholder });
   }
 
-  @LogExecution
   async clickButton(buttonName: string) {
     await this.buttonComponent.clickButtonByRole('button', buttonName);
   }
 
-  @LogExecution
   async checkCheckboxByLabel(label: string) {
     await this.checkboxComponent.checkByLabel(label);
   }
 
-  @LogExecution
   async checkCheckboxByRole(role: 'checkbox', name: string) {
     await this.context.getByRole(role, { name }).check({ force: true });
   }
