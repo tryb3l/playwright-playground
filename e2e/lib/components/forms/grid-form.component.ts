22e34037a32f97d2de776e9fee34e96f8b5f39f7
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-11-25 01:36:34 +0100

    refactor GridFormComponent to use type imports for Page

--- e2e/lib/components/forms/grid-form.component.ts
+++ e2e/lib/components/forms/grid-form.component.ts
@@ -1,4 +1,4 @@
-import { Page } from '@playwright/test';
+import type { Page } from '@playwright/test';
 import { FormComponent } from '@components/forms/base/base.form.component';
 
 class GridFormComponent extends FormComponent {
