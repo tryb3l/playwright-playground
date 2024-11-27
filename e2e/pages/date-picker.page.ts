dcb992dfb9efaea624dc26997068e7d5938456ea
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-11-27 13:36:39 +0100

    refactor DatePickerPage to utilize DatePickerComponent for date selection methods

--- e2e/pages/date-picker.page.ts
+++ e2e/pages/date-picker.page.ts
@@ -1,64 +1,27 @@
 import { expect, Page } from 'playwright/test';
+import { DatePickerComponent } from '@components/date-picker/date-picker.component';
 
 class DatePickerPage {
+  private datePickerComponent: DatePickerComponent;
+
   constructor(page: Page) {
-    this.page;
+    this.datePickerComponent = new DatePickerComponent(page);
   }
 
-  private async selectDateInCalendar(daysFromToday: number) {
-    let date = new Date();
-    date.setDate(date.getDate() + daysFromToday);
-    const expectedDay = date.getDate().toString();
-    const expectedMonthShort = date.toLocaleString('default', {
-      month: 'short',
-    });
-    const expectedMonthLong = date.toLocaleString('default', { month: 'long' });
-    const expectedYear = date.getFullYear().toString();
-    const dateToAssert = `${expectedMonthShort} ${expectedDay}, ${expectedYear}`;
-
-    let calendarMonthYear = await this.page
-      .locator('nb-calendar-view-mode')
-      .textContent();
-    const expectedMonthAndYear = `${expectedMonthLong} ${expectedYear}`;
-    while (
-      calendarMonthYear &&
-      !calendarMonthYear.includes(expectedMonthAndYear)
-    ) {
-      await this.page
-        .locator('nb-calendar-pageable-navigation [data-name="chevron-right"]')
-        .click();
-      calendarMonthYear = await this.page
-        .locator('nb-calendar-view-mode')
-        .textContent();
-    }
-    await this.page
-      .locator('.day-cell.ng-star-inserted')
-      .getByText(expectedDay, { exact: true })
-      .click();
-
-    return dateToAssert;
-  }
-
-  async selectCommonDatepickerDateFromToday(daysFromToday: number) {
-    const calendarInputField = await this.page.locator(
-      'input[placeholder="Form Picker"]'
+  async selectCommonDateFromToday(daysFromToday: number): Promise<void> {
+    await this.datePickerComponent.selectCommonDatepickerDateFromToday(
+      daysFromToday
     );
-    await calendarInputField.click();
-    const dateToAssert = await this.selectDateInCalendar(daysFromToday);
-
-    await expect(calendarInputField).toHaveValue(dateToAssert);
   }
 
-  async selectDatePickerRangeFromToday(
+  async selectDateRangeFromToday(
     daysFromToday: number,
     daysAfter: number
-  ) {
-    const calendarInputField = await this.page.getByPlaceholder('Range Picker');
-    await calendarInputField.click();
-    const dateFrom = await this.selectDateInCalendar(daysFromToday);
-    const dateTo = await this.selectDateInCalendar(daysAfter);
-
-    await expect(calendarInputField).toHaveValue(`${dateFrom} - ${dateTo}`);
+  ): Promise<void> {
+    await this.datePickerComponent.selectDatePickerRangeFromToday(
+      daysFromToday,
+      daysAfter
+    );
   }
 }
 
