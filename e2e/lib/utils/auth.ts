3caa735160a3235d9598c6db92ab8c555727f7c5
tryb3l <56679619+tryb3l@users.noreply.github.com>
2025-02-01 18:48:55 +0100

    small tweaks for the auth utils

--- e2e/lib/utils/auth.ts
+++ e2e/lib/utils/auth.ts
@@ -1,32 +1,33 @@
-import { Page } from '@playwright/test';
-import { BaseComponent } from '@components/base.component';
+import type { Page } from '@playwright/test';
+import { FormComponent } from '@components/forms/base/base.form.component';
+import { ButtonComponent } from '../components/button/button.component';
 import { Logger } from '@utils/logger';
 
-export class Authentication extends BaseComponent {
-    private logger: Logger;
+export class Authentication extends FormComponent {
+  protected logger: Logger;
+  protected buttonComponent: ButtonComponent;
 
-    constructor(page: Page){
-        super(page);
-        this.logger = new Logger('Authentication');
-    }
+  constructor(page: Page) {
+    super(page, 'form.ng-pristine.ng-invalid');
+    this.buttonComponent = new ButtonComponent(page, 'button');
+    this.logger = new Logger('Authentication');
+  }
 
-    async registerUser(email: string, password: string) {
-        this.logger.info(`Registering user: ${email}`);
-        await this.page.goto('/auth/register');
-        await this.fillInputByLabel('Email', email);
-        await this.fillInputByLabel('Password', password);
-        await this.clickButtonByText('Register');
-        await this.page.waitForNavigation();
-        this.logger.info(`User registered: ${email}`);
-      }
+  async registerUser(email: string, password: string) {
+    this.logger.info(`Registering user: ${email}`);
+    await this.page.goto('/auth/register');
+    await this.fillInputByLabel('Email', email);
+    await this.fillInputByLabel('Password', password);
+    await this.buttonComponent.clickButtonByText('Register');
+    this.logger.info(`User registered: ${email}`);
+  }
 
-      async loginUser(email: string, password: string) {
-        this.logger.info(`Logging in user: ${email}`);
-        await this.page.goto('/auth/login');
-        await this.fillInputByLabel('Email', email);
-        await this.fillInputByLabel('Password', password);
-        await this.clickButtonByText('Sign in');
-        await this.page.waitForNavigation();
-        this.logger.info(`User logged in: ${email}`);
-      }
+  async loginUser(email: string, password: string) {
+    this.logger.info(`Logging in user: ${email}`);
+    await this.page.goto('/auth/login');
+    await this.fillInputByLabel('Email', email);
+    await this.fillInputByLabel('Password', password);
+    await this.buttonComponent.clickButtonByText('Sign in');
+    this.logger.info(`User logged in: ${email}`);
+  }
 }
