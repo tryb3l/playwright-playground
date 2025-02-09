ef7ac12e26d06badf955dab820cb87ca0f4dd31a
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-02-10 00:53:23 +0100

    extend page mapper

--- e2e/lib/fixtures/page-mapping.ts
+++ e2e/lib/fixtures/page-mapping.ts
@@ -1,10 +1,12 @@
 import { FormLayoutsPage } from '@pages/form-layouts.page';
 import { DatePickerPage } from '@pages/date-picker.page';
+import { ToastrPage } from '@pages/toastr.page';
 import { StartPage } from '@fixtures/start-page.enum';
 
 export const startPageClassMap = {
   [StartPage.FormLayouts]: FormLayoutsPage,
   [StartPage.DatePicker]: DatePickerPage,
+  [StartPage.Toastr]: ToastrPage,
   //TODO: add more mappings
 };
 
