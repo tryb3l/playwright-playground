a2102dcaf214dc2895307e663f4b29b5b6eb613d
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-02-10 20:54:57 +0100

    refactor form component

--- e2e/lib/components/forms/base/base.form.component.ts
+++ e2e/lib/components/forms/base/base.form.component.ts
@@ -1,19 +1,41 @@
+// forms/base/base.form.component.ts
 import type { Page, Locator } from '@playwright/test';
 import { BaseComponent } from '@components/base.component';
 import { ButtonComponent } from '@components/button/button.component';
 import { CheckboxComponent } from '@components/checkbox/checkbox.component';
+import { RadioButtonComponent } from "@components/radio-button/radio-button.component"; // Import
 
 export abstract class FormComponent extends BaseComponent {
   protected formLocator: Locator;
-  protected buttonComponent: ButtonComponent;
-  protected checkboxComponent: CheckboxComponent;
+  private _buttonComponent: ButtonComponent | undefined;
+  private _checkboxComponent: CheckboxComponent | undefined;
+  private _radioButtonComponent: RadioButtonComponent | undefined;
 
   constructor(page: Page, formSelector: string) {
     super(page);
     this.formLocator = page.locator(formSelector);
-    this.context = this.formLocator;
-    this.buttonComponent = new ButtonComponent(page, formSelector);
-    this.checkboxComponent = new CheckboxComponent(page, formSelector);
+    this.context = this.formLocator; // Set form as context
+  }
+
+  protected get buttonComponent(): ButtonComponent {
+    if (!this._buttonComponent) {
+      this._buttonComponent = new ButtonComponent(this.context as Page, 'button'); // Use context and type assertion
+    }
+    return this._buttonComponent;
+  }
+
+  protected get checkboxComponent(): CheckboxComponent {
+    if (!this._checkboxComponent) {
+      this._checkboxComponent = new CheckboxComponent(this.context as Page, 'nb-checkbox')// Use context and type assertion
+    }
+    return this._checkboxComponent
+  }
+
+  protected get radioButtonComponent(): RadioButtonComponent {
+    if (!this._radioButtonComponent) {
+      this._radioButtonComponent = new RadioButtonComponent(this.context as Page, 'input[type="radio"]');
+    }
+    return this._radioButtonComponent;
   }
 
   async fillInputByRole(role: 'textbox', name: string, value: string) {
@@ -37,6 +59,10 @@ export abstract class FormComponent extends BaseComponent {
   }
 
   async checkCheckboxByRole(role: 'checkbox', name: string) {
-    await this.context.getByRole(role, { name }).check({ force: true });
+    await this.checkboxComponent.checkByRole(role, name)
+  }
+
+  async selectRadioButton(name: string) {
+    await this.radioButtonComponent.selectByRole('radio', name)
   }
 }
