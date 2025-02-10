f19848c3168e54e1a9d16b2c1ef6322f9fa15fe9
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-02-10 20:56:19 +0100

    refactor checkbox component, using lazyloading

--- e2e/lib/components/checkbox/checkbox.component.ts
+++ e2e/lib/components/checkbox/checkbox.component.ts
@@ -7,27 +7,37 @@ export class CheckboxComponent extends BaseComponent {
   constructor(page: Page, checkboxSelector: string) {
     super(page);
     this.checkboxLocator = page.locator(checkboxSelector);
-    this.context = this.checkboxLocator;
+  }
+
+  private getCheckbox(
+    options?: {
+      byRole?: Parameters<Page['getByRole']>[0];
+      byLabel?: Parameters<Page['getByLabel']>[0];
+      name?: string
+    }
+  ): Locator {
+    if (options?.byRole) {
+      return this.context.getByRole(options.byRole, { name: options.name });
+    } else if (options?.byLabel) {
+      return this.context.getByLabel(options.byLabel);
+    }
+    return this.checkboxLocator
   }
 
   async checkByRole(role: 'checkbox', name: string) {
-    const checkbox = this.getByRole(role, { name });
-    await checkbox.check({ force: true });
+    await this.getCheckbox({ byRole: role, name: name }).check({ force: true })
   }
 
   async checkByLabel(label: string) {
-    const checkbox = this.getByLabel(label);
-    await checkbox.check({ force: true });
+    await this.getCheckbox({ byLabel: label }).check({ force: true })
   }
 
   async uncheckByRole(role: 'checkbox', name: string) {
-    const checkbox = this.getByRole(role, { name });
-    await checkbox.uncheck({ force: true });
+    await this.getCheckbox({ byRole: role, name: name }).uncheck({ force: true })
   }
 
   async uncheckByLabel(label: string) {
-    const checkbox = this.getByLabel(label);
-    await checkbox.uncheck({ force: true });
+    await this.getCheckbox({ byLabel: label }).uncheck({ force: true })
   }
 
   async isChecked(): Promise<boolean> {
