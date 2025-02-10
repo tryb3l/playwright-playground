b9af27d5a346e7203e162dcd0958f87af36dcc66
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-02-10 19:33:23 +0100

    button component refactoring

--- e2e/lib/components/button/button.component.ts
+++ e2e/lib/components/button/button.component.ts
@@ -7,22 +7,21 @@ export class ButtonComponent extends BaseComponent {
   constructor(page: Page, buttonSelector: string) {
     super(page);
     this.buttonLocator = page.locator(buttonSelector);
-    this.context = this.buttonLocator;
   }
 
   async clickButtonByRole(role: 'button', name: string) {
-    await this.click(name, { byRole: role });
+    await this.click(this.buttonLocator, { byRole: role, name: name });
   }
 
   async clickButtonByLabel(label: string) {
-    await this.click(label, { byLabel: label });
+    await this.click(this.buttonLocator, { byLabel: label });
   }
 
   async clickButtonByText(text: string) {
-    await this.click(text, { text: text });
+    await this.click(this.buttonLocator, { text: text });
   }
 
   async clickButton(buttonName: string) {
-    await this.click(buttonName);
+    await this.click(this.buttonLocator);
   }
 }
