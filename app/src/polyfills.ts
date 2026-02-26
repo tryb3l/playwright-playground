ea3425f162b5440985257919c0b8edf6223a3a46
Bohdan Trybel <bogdan.trybel@gmail.com>
2026-02-26 23:18:23 +0100

    update polyfills and test configuration for Angular compatibility

--- app/src/polyfills.ts
+++ app/src/polyfills.ts
@@ -19,16 +19,7 @@
 */
 
 
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-import 'classlist.js';  // Run `npm install --save classlist.js`.
-
-/** IE10 and IE11 requires the following to support `@angular/animation`. */
-import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
-
-/** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+import 'zone.js';
 
 
 /** ALL Firefox browsers require the following to support `@angular/animation`. **/
@@ -45,12 +36,6 @@ import 'zone.js';  // Included with Angular CLI.
  * APPLICATION IMPORTS
  */
 
-/**
- * Date, currency, decimal and percent pipes.
- * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
- */
-import 'core-js/es7/array';
-import 'core-js/es7/object';
 
 if (typeof SVGElement.prototype.contains === 'undefined') {
   SVGElement.prototype.contains = HTMLDivElement.prototype.contains;
--- app/src/test.ts
+++ app/src/test.ts
@@ -6,12 +6,7 @@
 
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/dist/long-stack-trace-zone';
-import 'zone.js/dist/proxy.js';
-import 'zone.js/dist/sync-test';
-import 'zone.js/dist/jasmine-patch';
-import 'zone.js/dist/async-test';
-import 'zone.js/dist/fake-async-test';
+import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
@@ -22,13 +17,13 @@ import {
 declare const __karma__: any;
 
 // Prevent Karma from running prematurely.
-__karma__.loaded = function () {};
+__karma__.loaded = function () { };
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(), {
-    teardown: { destroyAfterEach: false },
+  teardown: { destroyAfterEach: false },
 },
 );
 // Finally, start Karma to run the tests.
